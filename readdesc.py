0f7c4f996fbb8bec53fa9ac46e0d6047aadce09d
Minh Nguyen <law.and.contradiction@gmail.com>
2012-10-18 21:20:31 +1100

    Added comments and additional variables.

--- readdesc.py
+++ readdesc.py
@@ -1,6 +1,6 @@
 import zipfile as zf # This is included in GMM as well, no need to include when merging.
-import os
-import StringIO
+import os            # Same with this one.
+import StringIO      # But I'll need to add these two.
 import urllib
 
 def openModDescription():
@@ -14,5 +14,9 @@ def openModDescription():
 
 words = openModDescription().readlines()
 modTitle = words[0].strip()
+modVersion = words[1].strip()
+modAuthor = words[2].strip()
 print words
 print modTitle
+print modVersion
+print modAuthor
